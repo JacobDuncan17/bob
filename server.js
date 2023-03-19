0d91d01ba2eeca121a4f1152a93221122a08ed1b
Jacob Duncan <duncan@Jacobs-MacBook-Pro.local>
2023-03-19 13:05:41 -0600

    added files and created the server.js file with the get routes

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js')
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET route for the home page
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listenting at http://localhost:${PORT}`)
+);
+
